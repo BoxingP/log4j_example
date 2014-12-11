85aa936d2553ce6dd761e2e11c6a836973af8136
Boxing <boxingp@gmail.com>
2014-12-11 09:28:23 +0800

    use log4j.xml to config

--- src/main/java/com/boxing/log4j/Log4jExample.java
+++ src/main/java/com/boxing/log4j/Log4jExample.java
@@ -1,7 +1,7 @@
 package com.boxing.log4j;
 
 import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.xml.DOMConfigurator;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -10,7 +10,7 @@ public class Log4jExample {
     static Logger logger = Logger.getLogger(Log4jExample.class.getName());
 
     public static void main(String[] args) throws IOException, SQLException {
-        PropertyConfigurator.configure("log4j.properties");
+        DOMConfigurator.configure("log4j.xml");
 
         logger.debug("Hello this is an debug message");
         logger.info("Hello this is an info message");
